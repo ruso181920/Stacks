901780797b622df6414e4a232eba8dc26a40ea16
Raushan Kumar <rraushan4590@gmail.com>
2026-02-06 17:25:12 +0530

    ReverseString

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ReverseString {
+ 
+ public static String reverseString(String str) {
+
+ 
+    Stack <Character> s= new Stack<> ();
+    int idx=0;
+    while (idx <str.length()) {
+        s.push(str.charAt(idx));
+        idx++;
+    }
+     
+    StringBuilder result =new StringBuilder ("");
+    while(!s.isEmpty ()) {
+        char curr =s.pop();
+        result.append(curr);
+
+    }
+    return result.toString();
+
+ }
+   
+   public static void main (String [] args) {
+     String str ="23456 rtt";
+     String result =reverseString(str);
+     System.out.println(result);
+   }
+
+}
+
